136258c1d7afdf67ed223a31192dd3bf4eb3f318
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-06-29 12:40:27 +0530

    feat: Add Find Minimum Time to Finish All Jobs

--- Array/Find Minimum Time to Finish All Jobs/python/solution.py
+++ Array/Find Minimum Time to Finish All Jobs/python/solution.py
@@ -1,12 +1,16 @@
 class Solution:
     def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
-        n = len(jobs)
-        if k == n:
-          return max(jobs)
-        else:
-            maxx = 0
-            for i in range(1,n-1):
-                cur = jobs[i-1] + jobs[i]
-                if cur <= n*2 + 1:
-                  maxx = max(maxx,cur)
-        return maxx        
+        low = max(jobs)
+        high = sum(jobs)
+
+        while low < high:
+            mid = low+(high-low)//2
+            if canWork(mid):
+                high = mid
+            else:
+                low = mid+1
+        def canWork(limit):
+            workers = [0]*k
+            def dfs(index):
+                
+        return dfs(0)                     
