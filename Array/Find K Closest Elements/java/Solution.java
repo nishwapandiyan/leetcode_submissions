d87b4a6ad75f4d0fe1ca67932e5493eda0898928
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-07-24 11:05:30 +0530

    feat: Add Find K Closest Elements

---
+++ Array/Find K Closest Elements/java/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+
+        ArrayList<Integer>list = new ArrayList<>();
+        int start = 0;
+        int end = arr.length - 1;
+        while (end - start >= k) {
+            if (Math.abs(arr[start] - x) > Math.abs(arr[end] - x)) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        for(int i=start; i<= end; i++){
+            list.add(arr[i]);
+        }
+        return list;
+    }
+}
