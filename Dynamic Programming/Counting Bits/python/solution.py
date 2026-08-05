b719ddb7080fc7efe593812c647955294cb319a0
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-08-05 23:22:21 +0530

    feat: Add Counting Bits

--- Dynamic Programming/Counting Bits/python/solution.py
+++ Dynamic Programming/Counting Bits/python/solution.py
@@ -2,5 +2,15 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         lst = []
         for i in range(n+1):
-            lst.append(bin(i).count('1'))
+            res = ""
+            if i ==0:
+                lst.append(i)
+            else:
+                ct = 0
+                while(i>0):
+                    rem = i%2
+                    res += str(rem)
+                    i //= 2
+                lst.append(res.count('1'))    
+
         return lst    
