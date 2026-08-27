e3128e2970c640bfaea0d77e8de7b24abe5fb501
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-08-27 15:49:21 +0530

    feat: Add Combinations

--- Backtracking/Combinations/python/solution.py
+++ Backtracking/Combinations/python/solution.py
@@ -1,6 +1,4 @@
 from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        op = list(range(1,n+1))
-        res = [x for x in combinations(op,k)]
-        return res
+        return list(combinations(range(1,n+1),k))
