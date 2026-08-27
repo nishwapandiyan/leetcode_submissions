65ab59d12c95cb3e55020253ccbf02b5c8ccbf8e
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-08-27 14:31:23 +0530

    feat: Add Swap Nodes in Pairs

---
+++ Linked List/Swap Nodes in Pairs/python/solution.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        dummy = ListNode(0,head)
+        dummy.next = head
+        prev = dummy
+
+        while prev.next and prev.next.next:
+            first = prev.next
+            second = first.next
+            nxt = second.next
+
+            second.next = first
+            first.next = nxt
+            prev.next = second
+            prev = first
+        return dummy.next     
